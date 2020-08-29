7238e1b1cea9a9f236ddf5dfeab3fed159e8e171
William Lin <william@192-168-1-111.tpgi.com.au>
2020-08-29 21:00:50 +1000

    getters and setters done

--- player.py
+++ player.py
@@ -12,10 +12,70 @@ class Player:
         self.__fgm = fgm
         self.__ftm = ftm 
         self.__tov = tov
-
-	def get_id(self):
-        return self.__id
-        
-    def get_x(self):
-        return self.__x
     
+    
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, p_id):
+        self.__id = p_id
+        
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_pts(self):
+        return self.__pts
+
+    def set_pts(self, pts):
+        self.__pts = pts
+
+    def get_reb(self):
+        return self.__reb
+
+    def set_reb(self, pts):
+        self.__reb = reb
+
+    def get_ast(self):
+        return self.__ast
+
+    def set_ast(self, pts):
+        self.__ast = ast
+    
+    def get_stls(self):
+        return self.__stls
+
+    def set_stls(self, pts):
+        self.__stls = stls
+    
+    def get_blks(self):
+        return self.__blks
+
+    def set_blks(self, pts):
+        self.__blks = blks
+    
+    def get_tpm(self):
+        return self.__tpm
+
+    def set_tpm(self, pts):
+        self.__tpm = tpm
+        
+    def get_fgm(self):
+        return self.__fgm
+
+    def set_fgm(self, pts):
+        self.__fgm = fgm
+
+    def get_ftm(self):
+        return self.__ftm
+
+    def set_ftm(self, pts):
+        self.__ftm = ftm
+
+    def get_tov(self):
+        return self.__tov
+
+    def set_tov(self, pts):
+        self.__tov = tov
