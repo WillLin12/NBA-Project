bbd6449e260a1cdd32ceec28f8cafb76dab9f1a3
William Lin <william@192-168-1-111.tpgi.com.au>
2020-08-29 19:35:33 +1000

    Added classes

---
+++ player.py
@@ -0,0 +1,104 @@
+class Player:
+    def __init__(self, id, name, pts, reb, ast, stls, blks, 3pm, fgm, ftm, tov):
+        self.id = id
+        self.name = name
+        self.pts = pts
+        self.reb = reb
+        self.ast = ast 
+        self.stls = stls
+        self.blks = blks
+        self.3pm = 3pm
+        self.fgm = fgm
+        self.ftm = ftm 
+        self.tov = tov
+
+	public = getId() {
+		return this.id;
+	}
+
+	public void setId(= id) {
+		this.id = id;
+	}
+
+	public = getName() {
+		return this.name;
+	}
+
+	public void setName(= name) {
+		this.name = name;
+	}
+
+	public = getPts() {
+		return this.pts;
+	}
+
+	public void setPts(= pts) {
+		this.pts = pts;
+	}
+
+	public = getReb() {
+		return this.reb;
+	}
+
+	public void setReb(= reb) {
+		this.reb = reb;
+	}
+
+	public = getAst() {
+		return this.ast;
+	}
+
+	public void setAst(= ast) {
+		this.ast = ast;
+	}
+
+	public = getStls() {
+		return this.stls;
+	}
+
+	public void setStls(= stls) {
+		this.stls = stls;
+	}
+
+	public = getBlks() {
+		return this.blks;
+	}
+
+	public void setBlks(= blks) {
+		this.blks = blks;
+	}
+
+	public = get3pm() {
+		return this.3pm;
+	}
+
+	public void set3pm(= 3pm) {
+		this.3pm = 3pm;
+	}
+
+	public = getFgm() {
+		return this.fgm;
+	}
+
+	public void setFgm(= fgm) {
+		this.fgm = fgm;
+	}
+
+	public = getFtm() {
+		return this.ftm;
+	}
+
+	public void setFtm(= ftm) {
+		this.ftm = ftm;
+	}
+
+	public = getTov() {
+		return this.tov;
+	}
+
+	public void setTov(= tov) {
+		this.tov = tov;
+	}
+
+
+
