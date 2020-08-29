ee1f933621edd064582dcdeaba8da9df33fbac05
William Lin <william@192-168-1-105.tpgi.com.au>
2020-08-30 00:18:42 +1000

    iterator and player changes

--- iterator.py
+++ iterator.py
@@ -1,15 +1,24 @@
 import csv
 import player as player
+import operator
 
 
 dict = {}
 
 with open('playerstats.csv') as player_stats:
     csv_reader = csv.reader(player_stats, delimiter=',')
+    next(csv_reader, None)
     line_count = 0
     for row in csv_reader:
-        p = player.Player(row[0], row[1], row[29], row[23], row[24], row[25], row[26], row[11], row[8], row[18], row[27], row[9], row[19])
+        p = player.Player(int(row[0]), row[1], float(row[29]), float(row[23]), float(row[24]), float(row[25]), float(row[26]), float(row[11]), float(row[8]), float(row[18]), float(row[27]), float(row[9]), float(row[19]))
         dict[p.get_id()] = p
+        #print(dict.keys())
 
-    for key, value in dict.items():
-        print (key, value.get_name())
+
+for value in (sorted(dict.values(), key=operator.attrgetter("pts"))):
+    print(value.get_name(), value.get_pts())
+
+# for key, value in dict.items():
+#     pid = float(key)
+#     if pid < 100:
+#         print (key, value.get_name(), value.get_pts())
--- player.py
+++ player.py
@@ -1,103 +1,103 @@
 class Player:
 
     def __init__(self, p_id, name, pts, reb, ast, stls, blks, tpm, fgm, ftm, tov, fga, fta):
-        self.__id = p_id 
-        self.__name = name
-        self.__pts = pts
-        self.__reb = reb
-        self.__ast = ast 
-        self.__stls = stls
-        self.__blks = blks
-        self.__tpm = tpm
-        self.__fgm = fgm
-        self.__fga = fga
-        self.__ftm = ftm 
-        self.__fta = fta
-        self.__tov = tov
+        self.id = p_id 
+        self.name = name
+        self.pts = pts
+        self.reb = reb
+        self.ast = ast 
+        self.stls = stls
+        self.blks = blks
+        self.tpm = tpm
+        self.fgm = fgm
+        self.fga = fga
+        self.ftm = ftm 
+        self.fta = fta
+        self.tov = tov
     
     def calculate_pct(self):
-        return self.__fgm / self.__fga
+        return self.fgm / self.fga
 
 
     def calculate_ftpct(self):
-        return self.__ftm / self.__fta
+        return self.ftm / self.fta
 
 
     
     def get_id(self):
-        return self.__id
+        return self.id
 
     def set_fta(self, p_id):
-        self.__id = p_id
+        self.id = p_id
 
     def get_fta(self):
-        return self.__id
+        return self.id
 
     def set_fga(self, p_id):
-        self.__id = p_id
+        self.id = p_id
 
     def get_fga(self):
-        return self.__id  
+        return self.id  
 
     def set_id(self, p_id):
-        self.__id = p_id
+        self.id = p_id
         
     def get_name(self):
-        return self.__name
+        return self.name
 
     def set_name(self, name):
-        self.__name = name
+        self.name = name
 
     def get_pts(self):
-        return self.__pts
+        return self.pts
 
     def set_pts(self, pts):
-        self.__pts = pts
+        self.pts = pts
 
     def get_reb(self):
-        return self.__reb
+        return self.reb
 
     def set_reb(self, pts):
-        self.__reb = reb
+        self.reb = reb
 
     def get_ast(self):
-        return self.__ast
+        return self.ast
 
     def set_ast(self, pts):
-        self.__ast = ast
+        self.ast = ast
     
     def get_stls(self):
-        return self.__stls
+        return self.stls
 
     def set_stls(self, pts):
-        self.__stls = stls
+        self.stls = stls
     
     def get_blks(self):
-        return self.__blks
+        return self.blks
 
     def set_blks(self, pts):
-        self.__blks = blks
+        self.blks = blks
     
     def get_tpm(self):
-        return self.__tpm
+        return self.tpm
 
     def set_tpm(self, pts):
-        self.__tpm = tpm
+        self.tpm = tpm
         
     def get_fgm(self):
-        return self.__fgm
+        return self.fgm
 
     def set_fgm(self, pts):
-        self.__fgm = fgm
+        self.fgm = fgm
 
     def get_ftm(self):
-        return self.__ftm
+        return self.ftm
 
     def set_ftm(self, pts):
-        self.__ftm = ftm
+        self.ftm = ftm
 
     def get_tov(self):
-        return self.__tov
+        return self.tov
 
     def set_tov(self, pts):
-        self.__tov = tov
+        self.tov = tov
